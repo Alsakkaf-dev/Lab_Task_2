91c7749fae4cf2e8b770f7a9e03d15f2d70bb56f
Alsakkaf.dev <mohammed.alsakkaaf@gmail.com>
2026-04-14 19:16:43 +0800

    script.js = refactoring so I  remove unnecessary comment and tidy up code structure and adding the fuctions updateCounter()

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let notes = []; 
 let nextId = 1;
 
-// 2. DOM REFERENCES
 const notesContainer = document.getElementById("notesContainer");
 const titleInput = document.getElementById("noteTitle");
 const contentInput = document.getElementById("noteContent");
@@ -10,3 +9,9 @@ const clearAllBtn = document.getElementById("clearAllBtn");
 const noteCounterSpan = document.getElementById("noteCounter");
 const windowSizeSpan = document.getElementById("windowSize");
 const userAgentSpan = document.getElementById("userAgent");
+
+
+function updateCounter() {
+    const count = notes.length;
+    noteCounterSpan.textContent = count === 1 ? "1 note" : `${count} notes`;
+}
